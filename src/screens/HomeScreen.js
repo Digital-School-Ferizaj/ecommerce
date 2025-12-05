eceac1122a7c36b4eed912394c88d1cca3f80f43
lirakH <lirakhamiti@hotmail.com>
2025-12-05 19:48:41 +0100

    poducts

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,73 +1,103 @@
 import  react from 'react';
 import { PRIMARY_COLOR } from '../components/variabla';
-import { StyleSheet, Text, View } from 'react-native';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList, ScrollView, Button} from 'react-native';
 import IconComp from '../components/IconComp';
+import ProductComp from '../components/ProductComp';
+import data from '../data/data.json';
 
 // import Swiper from 'react-native-swiper';
 
-export default function HomeScreen() {
-    return (
-        <View style={styles.container}>
+class HomeScreen extends react.Component {
+
+    constructor(){
+        super();
+        this.state = {
+            products: [],
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            products: data
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                
+                {/*
+                <View style={styles.sliderContainer}>
+                        <Swiper
+                        autoplay
+                        activeDotColor={PRIMARY_COLOR}
+                        autoplayTimeout={5}
+                    >
+                        <View style={styles.item}>
+                            <Image 
+                                source={require('../../assets/banner-4.jpg')}
+                                style={styles.imgItem}
+                                resizeMode="cover"
+                            />
+                        </View>
+                        <View style={styles.item}>
+                            <Image 
+                                source={require('../../assets/banner-2.jpg')}
+                                style={styles.imgItem}
+                                resizeMode="cover"
+                            />
+                        </View>
+                        <View style={styles.item}>
+                            <Image 
+                                source={require('../../assets/banner-3.jpg')}
+                                style={styles.imgItem}
+                                resizeMode="cover"
+                            />
+                        </View>
+                        <View style={styles.item}>
+                            <Image 
+                                source={require('../../assets/banner-6.jpg')}
+                                style={styles.imgItem}
+                                resizeMode="cover"
+                            />
+                        </View>
+                        <View style={styles.item}>
+                            <Image 
+                                source={require('../../assets/banner-1.jpg')}
+                                style={styles.imgItem}
+                                resizeMode="cover"
+                            />
+                        </View>
+                    </Swiper>
+                </View>
+                */}
+                
+                <View style={styles.iconsContainer}>
+                    <IconComp iconName="apple" iconText="Apple" />
+                    <IconComp iconName="android" iconText="Android" />
+                    <IconComp iconName="laptop" iconText="Laptop" />
+                </View>
+                <View style={styles.iconsContainer}>
+                    <IconComp iconName="tablet" iconText="Tablet" />                
+                    <IconComp iconName="mouse" iconText="Mouse" />
+                    <IconComp iconName="keyboard" iconText="Keyboard" />
+                </View>
+
+                <View style={styles.productsContainer}>
+                    <Text style={styles.title}>Most Popular Products</Text>
+                    <FlatList
+                        data={this.state.products.popularproducts}
+                        renderItem={({product}) => (
+                            <View>
+                                <ProductComp item={product} />
+                            </View>
+                        )}
+                    />
+                </View>
             
-            {/*
-            <View style={styles.sliderContainer}>
-                    <Swiper
-                    autoplay
-                    activeDotColor={PRIMARY_COLOR}
-                    autoplayTimeout={5}
-                >
-                    <View style={styles.item}>
-                        <Image 
-                            source={require('../../assets/banner-4.jpg')}
-                            style={styles.imgItem}
-                            resizeMode="cover"
-                        />
-                    </View>
-                    <View style={styles.item}>
-                        <Image 
-                            source={require('../../assets/banner-2.jpg')}
-                            style={styles.imgItem}
-                            resizeMode="cover"
-                        />
-                    </View>
-                    <View style={styles.item}>
-                        <Image 
-                            source={require('../../assets/banner-3.jpg')}
-                            style={styles.imgItem}
-                            resizeMode="cover"
-                        />
-                    </View>
-                    <View style={styles.item}>
-                        <Image 
-                            source={require('../../assets/banner-6.jpg')}
-                            style={styles.imgItem}
-                            resizeMode="cover"
-                        />
-                    </View>
-                    <View style={styles.item}>
-                        <Image 
-                            source={require('../../assets/banner-1.jpg')}
-                            style={styles.imgItem}
-                            resizeMode="cover"
-                        />
-                    </View>
-                </Swiper>
             </View>
-            */}
-            
-            <View style={styles.iconsContainer}>
-                <IconComp iconName="apple" iconText="Apple" />
-                <IconComp iconName="android" iconText="Android" />
-                <IconComp iconName="laptop" iconText="Laptop" />
-            </View>
-            <View style={styles.iconsContainer}>
-                <IconComp iconName="tablet" iconText="Tablet" />                
-                <IconComp iconName="mouse" iconText="Mouse" />
-                <IconComp iconName="keyboard" iconText="Keyboard" />
-            </View>
-        </View>
-    );
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -112,4 +142,4 @@ const styles = StyleSheet.create({
 });
 
 
-export { HomeScreen };
+export default HomeScreen;
